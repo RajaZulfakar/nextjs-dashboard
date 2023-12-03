a0aff0dcda89886415b43b860bd826a3ccda831b
raja <raja@192.168.1.8>
2023-12-03 19:17:09 +0700

    Versioning untracked files

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,7 +2,5 @@ import {Inter, Lusitana} from 'next/font/google'
 
 //export fonts 
 export const inter = Inter ({subsets: ['latin']})
-export const lusitana = Lusitana ({
-    weight:['400', '700'],
-    subsets:['latin']
-})
+export const lusitana = Lusitana({ weight:'400' ,subsets: ['latin'] 
+});
